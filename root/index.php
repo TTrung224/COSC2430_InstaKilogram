34e5c358289cb7da1eef3f95c3042c45a693e70e
s3877653 <hotranminhkhoi@gmail.com>
2022-04-28 18:07:18 +0700

    add footer

--- root/index.php
+++ root/index.php
@@ -10,11 +10,43 @@
         main section
         <?php require_once('templates/feed.php'); ?>
         <a href="functions/logout.php">Logout</a>
+        <p> uh </p>
+        <p> uh </p>
+        <p> uh </p>
+        <p> uh </p>
+        <p> uh </p>
+        <p> uh </p>
+        <p> uh </p>
+        <p> uh </p>
+        <p> uh </p>
+        <p> uh </p>
+        <p> uh </p>
+        <p> uh </p>
+        <p> uh </p>
+        <p> uh </p>
+        <p> uh </p>
+        <p>  </p>
+        <p>  </p>
+        <p>  </p>
+        <p>  </p>
+        <p> </p>
+        <p>  </p>
+        <p> </p>
+        <p> </p>
+        <p>  </p>
+
     </main>
 
     <!--footer-->
     <footer>
-
+        <div class="footer-logo"><img src="Assets\InstaKilogram_logo.jpeg" alt="footer-logo"></div>
+        <div class="footer-copyright">
+            <p class="footer-copyright">2022 © Copyright COSC2430_A2 Group 4. All rights reserved.</p>
+        </div>
+        <div class="other">
+            <a href="">About</a> | <a href="">Privacy</a> | <a href="">Help</a>
+        </div>
+        
     </footer>
 </body>
 </html>
