d95b12b57c4d85bee9e48f917032f2a4d764e0e5
s3877653 <hotranminhkhoi@gmail.com>
2022-04-28 18:20:28 +0700

    add footer style

--- root/index.php
+++ root/index.php
@@ -39,12 +39,14 @@
 
     <!--footer-->
     <footer>
-        <div class="footer-logo"><img src="Assets\InstaKilogram_logo.jpeg" alt="footer-logo"></div>
+        <div class="footer-logo"><a href="index.php"><img src="Assets\InstaKilogram_logo.jpeg" alt="footer-logo"></a></div>
         <div class="footer-copyright">
             <p class="footer-copyright">2022 © Copyright COSC2430_A2 Group 4. All rights reserved.</p>
         </div>
         <div class="other">
-            <a href="">About</a> | <a href="">Privacy</a> | <a href="">Help</a>
+            <a href="">About</a>
+            <a href="">Privacy</a>
+            <a href="">Help</a>
         </div>
         
     </footer>
