5b782f36ff8fdb1954acac30063fb8bc2aa5c79f
Trung Tran <s3891724@rmit.edu.vn>
2022-05-13 14:30:11 +0700

    add responsive for the feed and footer

--- root/profile_page.php
+++ root/profile_page.php
@@ -12,6 +12,7 @@
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <link rel="stylesheet" href="style.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
       <title>Profile</title>
   </head>
   <body>
@@ -55,5 +56,8 @@
       </div>
       <!-- End of container -->
     </main>
+
+    <!--footer-->
+    <?php require_once('templates/footer.php'); ?>
   </body>
 </html>
